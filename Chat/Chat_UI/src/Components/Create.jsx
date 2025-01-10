177385fbd3b3c9112e020b2a0714c4dc864082cd
Avinash Sharma <Avinashsharma31384@gmail.com>
2025-01-10 16:36:42 +0530

    After some updation in chat

--- Chat/Chat_UI/src/Components/Create.jsx
+++ Chat/Chat_UI/src/Components/Create.jsx
@@ -1,10 +1,8 @@
-
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Create() {
-
     const [from, setFrom] = useState("");
     const [to, setTo] = useState("");
     const [message, setMessage] = useState("");
@@ -12,39 +10,71 @@ function Create() {
     const navigate = useNavigate();
     const handleSubmit = () => {
         // e.preventDefault();
-        axios.post("http://localhost:4000/Create", {
-            from,
-            to,
-            message
-        })
-        .then((response)=>{
-            console.log(response);
-            setFrom("");
-            setTo("");
-            setMessage("");
-            navigate("/Create", { replace: true });
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
+        axios
+            .post("http://localhost:4000/Create", {
+                from,
+                to,
+                message,
+            })
+            .then((response) => {
+                console.log(response);
+                setFrom("");
+                setTo("");
+                setMessage("");
+                navigate("/Create", { replace: true });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
-  return (
-    <div className="container w-full  bg-cyan-900 flex justify-center items-center ">
-        <form action="" className="w-1/3 h-1/2 bg-slate-600 p-20 text-white flex justify-center flex-col">
-        <h1 className="text-yellow-500 text-xl" >Enter Data</h1>
-           <label htmlFor="">From</label>
-           <input className="block p-1 text-black outline-none" type="text" name="" id="" onChange={(e)=>{setFrom(e.target.value)}} />
-           <label htmlFor="">To</label>
-           <input className="block p-1 text-black outline-none" type="text" name="" id="" onChange={(e)=>{setTo(e.target.value)}} />
-           <label htmlFor="">Message</label>
-           <input className="block p-1 text-black outline-none" type="text" name="" id="" onChange={(e)=>{
-            setMessage(e.target.value)
-           }} />
-           <button type="submit" className="bg-red-600 mt-2" onClick={handleSubmit}>Submit</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className="container w-full  bg-cyan-900 flex justify-center items-center ">
+            <form
+                action=""
+                className="w-1/3 h-1/2 bg-slate-600 p-20 text-white flex justify-center flex-col"
+            >
+                <h1 className="text-yellow-500 text-xl">Enter Data</h1>
+                <label htmlFor="">From</label>
+                <input
+                    className="block p-1 text-black outline-none"
+                    type="text"
+                    name=""
+                    id=""
+                    onChange={(e) => {
+                        setFrom(e.target.value);
+                    }}
+                />
+                <label htmlFor="">To</label>
+                <input
+                    className="block p-1 text-black outline-none"
+                    type="text"
+                    name=""
+                    id=""
+                    onChange={(e) => {
+                        setTo(e.target.value);
+                    }}
+                />
+                <label htmlFor="">Message</label>
+                <input
+                    className="block p-1 text-black outline-none"
+                    type="text"
+                    name=""
+                    id=""
+                    onChange={(e) => {
+                        setMessage(e.target.value);
+                    }}
+                />
+                <button
+                    type="submit"
+                    className="bg-red-600 mt-2"
+                    onClick={handleSubmit}
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 }
 
-export default Create
+export default Create;
--- Chat/Chat_UI/src/Components/Pages/Nav.jsx
+++ Chat/Chat_UI/src/Components/Pages/Nav.jsx
@@ -1,7 +1,7 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 function Nav() {
-  return (
-    <>
+    return (
+        <>
             {/* Header  */}
             <header className="sticky top-0 left-0">
                 <nav className="bg-slate-600 w-full h-[60px] flex justify-around items-center text-white ">
@@ -10,16 +10,24 @@ function Nav() {
                     </div>
                     <div className="link">
                         <ul className="flex">
-                            <NavLink to="/"><li className="p-3 text-lg" >Home</li></NavLink>
-                            <NavLink to="/About"><li className="p-3 text-lg" >About</li></NavLink>
-                            <NavLink to="/Contact"><li className="p-3 text-lg" >Contact</li></NavLink>
-                            <NavLink to="/Services"><li className="p-3 text-lg" >All Chats</li></NavLink>
+                            <NavLink to="/">
+                                <li className="p-3 text-lg">Home</li>
+                            </NavLink>
+                            <NavLink to="/About">
+                                <li className="p-3 text-lg">About</li>
+                            </NavLink>
+                            <NavLink to="/Contact">
+                                <li className="p-3 text-lg">Contact</li>
+                            </NavLink>
+                            <NavLink to="/AllChat">
+                                <li className="p-3 text-lg">All Chats</li>
+                            </NavLink>
                         </ul>
                     </div>
                 </nav>
             </header>
-    </>
-  )
+        </>
+    );
 }
 
-export default Nav
+export default Nav;
